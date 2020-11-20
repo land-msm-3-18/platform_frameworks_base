0d0fdb78ec31281534d44ab1b8295a22609a70b9
Ido Ben-Hur <idoybh2@gmail.com>
2020-11-20 18:43:26 +0200

    AudioService: Cancel old toasts when switching ringer mode
    When the user cycles fast between modes the last toast will show so late it'll be irrelevant
    
    Change-Id: I0f8f442ea10d670663fdd298f2198f6e8baa7a36
    Signed-off-by: Dmitrii <bankersenator@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- services/core/java/com/android/server/audio/AudioService.java
+++ services/core/java/com/android/server/audio/AudioService.java
@@ -4280,6 +4280,8 @@ public class AudioService extends IAudioService.Stub
         setRingerMode(ringerMode, caller, false /*external*/);
     }
 
+    private static Toast mSilenceToast;
+
     public void silenceRingerModeInternal(String reason) {
         VibrationEffect effect = null;
         int ringerMode = AudioManager.RINGER_MODE_SILENT;
@@ -4333,7 +4335,9 @@ public class AudioService extends IAudioService.Stub
         setRingerModeInternal(ringerMode, reason);
         if (ringerMode == AudioManager.RINGER_MODE_NORMAL)
             playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
-        Toast.makeText(mContext, toastText, Toast.LENGTH_SHORT).show();
+        if (mSilenceToast != null) mSilenceToast.cancel();
+        mSilenceToast = Toast.makeText(mContext, toastText, Toast.LENGTH_SHORT);
+        mSilenceToast.show();
     }
 
     private boolean maybeVibrate(VibrationEffect effect, String reason) {
